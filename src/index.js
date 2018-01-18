cf55b3f1e6bbc37a28c2d53b8a794a0a35600793
Donovan Maxwell <donovan.maxwell@gmail.com>
2018-01-17 18:02:25 -0600

    added eslint and eslint-watch

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const devValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${devValue} for this awesome starter kit!`);
+console.log(`I would pay ${devValue} for this awesome starter kit!`); // eslint-disable-line no-console
